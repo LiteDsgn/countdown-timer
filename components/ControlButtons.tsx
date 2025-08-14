c18a1d77b518307e06d7150e35a8f38fcb23aec8
LiteDsgn <Litedsgnstudio@gmail.com>
2025-08-14 17:52:55 +0100

    style(ControlButtons): replace size prop with className for icons
    Use tailwind classes for consistent icon sizing instead of size prop

--- components/ControlButtons.tsx
+++ components/ControlButtons.tsx
@@ -33,14 +33,14 @@ const ControlButtons: React.FC<ControlButtonsProps> = ({
         disabled={isRunning}
         className="p-4 sm:p-6 rounded-lg border-2 border-white/20 hover:border-white/40 hover:bg-white/10 transform hover:scale-105 transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
       >
-        <Minus size={24} sm:size={32} />
+        <Minus className="w-6 h-6 sm:w-8 sm:h-8" />
       </button>
       <button
         onClick={onIncrement}
         disabled={isRunning}
         className="p-4 sm:p-6 rounded-lg border-2 border-white/20 hover:border-white/40 hover:bg-white/10 transform hover:scale-105 transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
       >
-        <Plus size={24} sm:size={32} />
+        <Plus className="w-6 h-6 sm:w-8 sm:h-8" />
       </button>
       {!isRunning ? (
         <button
@@ -55,21 +55,21 @@ const ControlButtons: React.FC<ControlButtonsProps> = ({
           disabled={timeLeft === 0 && !isPomodoroMode}
           className="p-4 sm:p-6 rounded-lg bg-green-500 hover:bg-green-600 disabled:opacity-50 disabled:cursor-not-allowed transform hover:scale-105 transition-all duration-300"
         >
-          <Play size={24} sm:size={32} />
+          <Play className="w-6 h-6 sm:w-8 sm:h-8" />
         </button>
       ) : (
         <button
           onClick={onPause}
           className="p-4 sm:p-6 rounded-lg bg-red-500 hover:bg-red-600 transform hover:scale-105 transition-all duration-300"
         >
-          <Pause size={24} sm:size={32} />
+          <Pause className="w-6 h-6 sm:w-8 sm:h-8" />
         </button>
       )}
       <button
         onClick={onReset}
         className="p-4 sm:p-6 rounded-lg border-2 border-white/20 hover:border-white/40 hover:bg-white/10 transform hover:scale-105 transition-all duration-300"
       >
-        <RotateCcw size={24} sm:size={32} />
+        <RotateCcw className="w-6 h-6 sm:w-8 sm:h-8" />
       </button>
     </div>
   )
